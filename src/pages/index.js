77b9f1dd044b70395ffbd627962f1a46c6d8ac83
Moein Alizadeh <truemoein@gmail.com>
2019-05-12 05:50:00 +0430

    :art: Improve styles.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import '../assets/scss/main.css'
 import logo from "./../images/ThingersLogo.png"
 
 const IndexPage = () => (
@@ -16,6 +17,8 @@ const IndexPage = () => (
     <img width={300} src={logo} alt="Thingers.ir" />
     <h1
       style={{
+        textAlign: "center",
+        lineHeight: 1.7,
         fontFamily: "fantasy",
         color: "rgba(23, 49, 62, 0.9)",
         textShadow: "0px 6px 5px rgba(0, 0, 0, 0.21)",
