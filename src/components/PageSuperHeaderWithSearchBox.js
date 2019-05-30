330e66321717b09c070167769e0dd20af080448f
TrueMoein <truemoein@gmail.com>
2019-05-30 05:24:52 +0430

    Add uncommited PageSuperHeaderWithSearchBox.

---
+++ src/components/PageSuperHeaderWithSearchBox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Img from "gatsby-image"
+import SearchBox from "./SearchBox"
+
+export default function PageSuperHeaderWithSearchBox(props) {
+  const { image, imageAlt } = props;
+  return (
+    <div className="container-fluid">
+      <div className="row page-super-header justify-content-center align-items-center">
+        <Img fixed={image} alt={imageAlt}/>
+        <div className="gradient-layer" />
+        <div className="search-in-header-container col-md-4 col-xs-12">
+          <SearchBox />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+PageSuperHeaderWithSearchBox.propTypes = {
+  image: PropTypes.object,
+  imageAlt: PropTypes.string
+}
